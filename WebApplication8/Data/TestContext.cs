db83b9a5b250831a10ae7f62b45c4ee62a0bab41
yev <yev.berezovsky@gmail.com>
2019-08-16 09:24:41 -0400

    initial commit

---
+++ WebApplication8/Data/TestContext.cs
@@ -0,0 +1,16 @@
+﻿using WebApplication8.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace WebApplication8.Data
+{
+    public class SchooTestContext : DbContext
+    {
+        public SchooTestContext(DbContextOptions<SchooTestContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Enrollment> Enrollments { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
